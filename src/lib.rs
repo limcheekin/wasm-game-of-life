cc3ae940463643d987e2b7a7c59dbf77acee1274
Lim Chee Kin <limcheekin@vobject.com>
2021-06-23 14:37:01 +0800

    test: added logs

--- src/lib.rs
+++ src/lib.rs
@@ -57,7 +57,12 @@ impl Universe {
                 let idx = self.get_index(row, col);
                 let cell = self.cells[idx];
                 let live_neighbors = self.live_neighbor_count(row, col);
-
+                log!("cell[{}, {}] is initially {:?} and has {} live neighbors",
+                      row,
+                      col,
+                      cell,
+                      live_neighbors
+                    );
                 let next_cell = match (cell, live_neighbors) {
                     // Rule 1: Any live cell with fewer than two live neighbours
                     // dies, as if caused by underpopulation.
@@ -75,6 +80,8 @@ impl Universe {
                     (otherwise, _) => otherwise,
                 };
 
+                log!("    it becomes {:?}", next_cell);
+                
                 next[idx] = next_cell;
             }
         }
@@ -83,6 +90,8 @@ impl Universe {
     }
 
     pub fn new() -> Universe {
+        utils::set_panic_hook();
+
         let width = 64;
         let height = 64;
 
@@ -169,5 +178,15 @@ impl Universe {
     }
 
 }
+
+extern crate web_sys;
+
+// A macro to provide `println!(..)`-style syntax for `console.log` logging.
+macro_rules! log {
+    ( $( $t:tt )* ) => {
+        web_sys::console::log_1(&format!( $( $t )* ).into());
+    }
+}
+
 
 
